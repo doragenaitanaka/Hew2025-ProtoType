4e2a26c5b244d2485481e8cbef6db1b5cc8710c8
Klbs-delicious <takahashi.kaho.30677@gmail.com>
2024-12-18 16:02:49 +0900

    配列用の解放マクロの追加
    ・new Object[3]とかのポインタの解放用マクロ

--- Hew2025_ProtoType/Library/Code/self/SafePointers.h
+++ Hew2025_ProtoType/Library/Code/self/SafePointers.h
@@ -3,4 +3,5 @@
 #define SAFE_RELEASE(pointer) { if( nullptr != pointer ) { pointer->Release(); pointer = nullptr; } }
 
 // 解放用の簡略化マクロ
-#define SAFE_DELETE(pointer) { if( nullptr != pointer ) {delete pointer; pointer=nullptr;  } }
+#define SAFE_DELETE(pointer) { if( nullptr != pointer ) {delete pointer; pointer=nullptr;  } }				// 単体で確保していた場合
+#define SAFE_DELETE_ARRAY(pointer) { if( nullptr != pointer ) {delete[] pointer; pointer=nullptr;  } }		// 配列で確保していた場合
