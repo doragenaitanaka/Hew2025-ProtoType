3f53ff11f786bb57f2aaac2ed50a9534dcdf01ad
Reins8834 <reins8834team@gmail.com>
2025-01-20 19:44:33 +0900

    Stage11の実装
    Stage11の実装
    一部当たり判定のない場所があるが通常プレイでは触れることのできない場所のため問題は無いと思われ
    
    プレイヤー色の変化
    床 = 赤
    フック = 紫
    ゴール = 黄色
    振り子(左右で色が違う) = 緑orオレンジ

--- Hew2025_ProtoType/Library/Code/self/01_GameManager/GameManager.cpp
+++ Hew2025_ProtoType/Library/Code/self/01_GameManager/GameManager.cpp
@@ -7,7 +7,7 @@
 
 //==================最初に実行されるシーン(ここしかいじっちゃダメ！！！！！！)========================
 
-Scene GameManager::startScene = Scene::Stage_6;
+Scene GameManager::startScene = Scene::Stage_11;
 
 //====================================================================================================
 GameManager* GameManager::gameManager = nullptr;
