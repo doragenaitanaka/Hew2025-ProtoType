97df11aa029fae22998d399aed2cd03c972dceaf
Klbs-delicious <takahashi.kaho.30677@gmail.com>
2025-01-28 18:48:42 +0900

    ステージセレクトをコントローラーに対応
    ・L1、R1をキーコンフィグに登録

--- Hew2025_ProtoType/Library/Code/self/01_GameManager/GameManager.cpp
+++ Hew2025_ProtoType/Library/Code/self/01_GameManager/GameManager.cpp
@@ -7,7 +7,7 @@
 
 //==================最初に実行されるシーン(ここしかいじっちゃダメ！！！！！！)========================
 
-Scene GameManager::startScene = Scene::Stage_6;
+Scene GameManager::startScene = Scene::StageSelectScene;
 
 //====================================================================================================
 GameManager* GameManager::gameManager = nullptr;
