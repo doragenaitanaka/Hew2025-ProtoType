4600b5a74a3c2c0cbc30aa4f6257d9038f2beb1d
Rikodaku <342605959@qq.com>
2025-01-29 14:13:21 +0900

    Stage2

--- Hew2025_ProtoType/Library/Code/self/01_GameManager/GameManager.cpp
+++ Hew2025_ProtoType/Library/Code/self/01_GameManager/GameManager.cpp
@@ -7,7 +7,7 @@
 
 //==================最初に実行されるシーン(ここしかいじっちゃダメ！！！！！！)========================
 
-Scene GameManager::startScene = Scene::TitleScene;
+Scene GameManager::startScene = Scene::Stage_2;
 
 //====================================================================================================
 GameManager* GameManager::gameManager = nullptr;
