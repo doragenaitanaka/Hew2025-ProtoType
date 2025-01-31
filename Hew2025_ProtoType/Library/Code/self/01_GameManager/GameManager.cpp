416565a41e3b1516b90ba1eafc88468b9e4c3bf9
ドラゲナイ田中 <neconoob@gmail.com>
2025-01-31 17:53:56 +0900

    st4 ld

--- Hew2025_ProtoType/Library/Code/self/01_GameManager/GameManager.cpp
+++ Hew2025_ProtoType/Library/Code/self/01_GameManager/GameManager.cpp
@@ -7,7 +7,7 @@
 
 //==================最初に実行されるシーン(ここしかいじっちゃダメ！！！！！！)========================
 
-Scene GameManager::startScene = Scene::Stage_4;
+Scene GameManager::startScene = Scene::TitleScene;
 
 //====================================================================================================
 GameManager* GameManager::gameManager = nullptr;
